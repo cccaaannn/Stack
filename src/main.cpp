cbcabc6074fe8b7ce6199a13e65303d6f54013ef
cccaaannn <can.kurt.aa@gmail.com>
2021-05-31 20:01:39 +0300

    main file updated

--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,9 @@
 
 int main() {
 
-    Stack<int> stack;
-
-    std::cout << "Pushing elements to stack" << std::endl;
-
-    stack.push(10);
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-    stack.push(20);
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-    stack.push(30);
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-    stack.push(40);
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-    stack.push(50);
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-    stack.push(60);
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-    stack.push(70);
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-
-    std::cout << std::endl << "Printing stack" << std::endl;
-    std::cout << stack << std::endl;
-
-    std::cout << std::endl << "Poping elements from stack" << std::endl;
-
-    stack.pop();
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-    stack.pop();
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-    stack.pop();
-    std::cout << "top: " << stack.top() << " size: " << stack.size() << std::endl;
-
+    example1();
+    // example2();
 
     system("pause");
     return 0;
 }
-
